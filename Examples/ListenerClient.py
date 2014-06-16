a7606e78caad1488f212d7c81751fa1cda5f680d
Danilo Vaz <danilovazb@hotmail.com>
2014-06-16 17:10:43 -0300

    Enviando

---
+++ Examples/ListenerClient.py
@@ -0,0 +1,86 @@
+#coding: utf8 
+'''
+Copyright (c) <2012> Tarek Galal <tare2.galal@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so, subject to the following 
+conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR 
+A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir)
+import datetime, sys
+
+if sys.version_info >= (3, 0):
+	raw_input = input
+
+from Yowsup.connectionmanager import YowsupConnectionManager
+
+#password = "8LVUOD5ZDxJIRP1rF5zFdl1js60="                                    #Password dada ao registrar o numero pelo yowsup.
+#password = base64.b64decode(bytes(password.encode('utf-8')))    #Codificacao do Password para envio aos servidores do whatsApp.
+#username = '5511949152392'                                      #Numero de telefone para o inicio de secao
+keepAlive= False                                                #Conexao persistente com o servidor.
+
+class WhatsappListenerClient:
+	
+	def __init__(self, keepAlive = False, sendReceipts = False):
+		self.sendReceipts = sendReceipts
+		
+		connectionManager = YowsupConnectionManager()
+		connectionManager.setAutoPong(keepAlive)
+
+		self.signalsInterface = connectionManager.getSignalsInterface()
+		self.methodsInterface = connectionManager.getMethodsInterface()
+		
+		self.signalsInterface.registerListener("message_received", self.onMessageReceived)
+		self.signalsInterface.registerListener("auth_success", self.onAuthSuccess)
+		self.signalsInterface.registerListener("auth_fail", self.onAuthFailed)
+		self.signalsInterface.registerListener("disconnected", self.onDisconnected)
+		
+		self.cm = connectionManager
+	
+	def login(self, username, password):
+		self.username = username
+		self.methodsInterface.call("auth_login", (username, password))
+		
+		
+		while True:
+			raw_input()	
+
+	def onAuthSuccess(self, username):
+		print("Authed %s" % username)
+		self.methodsInterface.call("ready")
+
+	def onAuthFailed(self, username, err):
+		print("Auth Failed!")
+
+	def onDisconnected(self, reason):
+		print("Disconnected because %s" %reason)
+		#wa = WhatsappListenerClient(args['keepalive'], args['autoack'])
+		#wa.login(login, password)	
+		
+	def onMessageReceived(self, messageId, jid, messageContent, timestamp, wantsReceipt, pushName, isBroadCast):
+		formattedDate = datetime.datetime.fromtimestamp(timestamp).strftime('%d-%m-%Y %H:%M')
+		print("%s [%s]:%s"%(jid, formattedDate, messageContent))
+
+		#if messageContent == 'buceta':
+		#	print("lol")
+		#	os.system('python /home/danilo/c0d3r/Python/yowsup/src/yowsup-cli --send 5511948918069 "Nem sabe o que é e fica mandando no meu whatsapp, vai se fuder viado!! kkkk" --wait --config chip3.config')
+
+		if wantsReceipt and self.sendReceipts:
+			self.methodsInterface.call("message_ack", (jid, messageId))
+	
